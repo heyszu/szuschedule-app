3294ed2f6db91120f58458d90e9d9206f9942b5d
Jas0ncn <ceocjy@vip.qq.com>
2016-11-09 23:38:04 +0800

    update: vuex

--- app/src/vuex/actions.js
+++ app/src/vuex/actions.js
@@ -1,23 +1,9 @@
-import config from '../config'
-import Vue from 'vue'
+// import config from '../config'
+// import Vue from 'vue'
 
 module.exports = {
-    updateSchedule ({ commit }, ticket) {
-        return new Promise(function (resolve, reject) {
-            Vue.http.post(config.api + '/schedule', {
-                ticket
-            }).then(res => {
-                if (res.data.status === 200) {
-                    commit('SCHEDULE', res.data.data.schedule)
-                    resolve()
-                } else {
-                    reject()
-                }
-            }, e => {
-                console.log(e)
-                reject()
-            })
-        })
+    updateSchedule ({ commit }, schedule) {
+        commit('SCHEDULE', schedule)
     },
     updatePreLoginState ({ commit }, data) {
         commit('PRELOGIN', data)
--- app/src/vuex/mutations.js
+++ app/src/vuex/mutations.js
@@ -1,10 +1,17 @@
 export function SCHEDULE (state, scheduleData) {
-    updateSchedule(state, scheduleData)
+    // updateSchedule(state, scheduleData)
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleData))
+    state.schedule = scheduleData
 }
 
 export function PRELOGIN (state, data) {
-    updatePreLoginState(state, data)
+    // updatePreLoginState(state, data)
+    state.isPreLogin = data.isPreLogin
+    state.preLoginState = data.preLoginState
+    state.schedule = data.schedule
 }
+
+/*
 
 function updateSchedule (state, scheduleData) {
     window.localStorage.setItem('schedule', JSON.stringify(scheduleData))
@@ -16,3 +23,5 @@ function updatePreLoginState (state, data) {
     state.preLoginState = data.preLoginState
     state.schedule = data.schedule
 }
+
+*/
