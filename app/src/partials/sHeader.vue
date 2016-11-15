376d1aff800803c2063756090fb72ad9edc70a3a
Jas0ncn <ceocjy@vip.qq.com>
2016-11-15 16:56:19 +0800

    feature: add `logout` and `about` (close #5,#6)

--- app/src/partials/sHeader.vue
+++ app/src/partials/sHeader.vue
@@ -6,9 +6,15 @@
                 {{ (weekOdd === 2) ? '双周' : '单周' }} <i class="material-icons">arrow_drop_down</i>
             </div>
             <div class="rightBtn">
-                <i class="material-icons">more_vert</i>
+                <i class="material-icons" @touchstart="toggleMenu">more_vert</i>
+                <div class="menu" :class="{ menuOpen: menuVisible }">
+                    <!--<li @touchstart="backup">备份/恢复</li>-->
+                    <li @touchstart="logout">退出账号</li>
+                    <li @touchstart="about">关于</li>
+                </div>
             </div>
         </div>
+        <div class="mask" v-show="menuVisible" @touchstart="toggleMenu"></div>
     </header>
 </template>
 
@@ -17,7 +23,8 @@ export default {
     props: ['weekOdd'],
     data () {
         return {
-            isAndroid: false
+            isAndroid: false,
+            menuVisible: false
         }
     },
     mouted () {
@@ -36,60 +43,28 @@ export default {
     methods: {
         switchSingleWeek () {
             this.$emit('switchSingleWeek')
+        },
+        toggleMenu () {
+            this.menuVisible = !this.menuVisible
+        },
+        backup () {
+            console.log('backup')
+            this.toggleMenu()
+        },
+        logout () {
+            this.toggleMenu()
+            window.localStorage.removeItem('isPreLogin')
+            window.localStorage.removeItem('isLogin')
+            window.location.reload()
+        },
+        about () {
+            this.toggleMenu()
+            navigator.notification.alert(`深大课表是由“这很深大”公众号团队开发的一款课表查询软件，仅供深大学生使用。\n版本号: v0.2.5 preview\n作者: Jason <jason@iszu.cn>`, function () {}, '关于深大课表', '知道了')
         }
     }
 }
 </script>
 
 <style lang="less" scoped>
-* {
-    display: flex;
-}
-
-header {
-    position: fixed;
-    top: 0;
-    left: 0;
-
-    width: 100%;
-    height: 76px;
-
-    background: #FFF;
-
-    color: #333;
-
-    flex-direction: column;
-
-    z-index: 100;
-
-    div {
-        padding: 10px;
-
-        align-items: center;
-
-        background: #FFF;
-    }
-
-    .leftBtn {
-        width: 60%;
-        padding-left: 20px;
-
-        font-size: 16px;
-
-        .material-icons {
-            font-size: 24px;
-        }
-    }
-
-    .rightBtn {
-        width: 40%;
-        padding-right: 20px;
-
-        justify-content: flex-end;
-
-        .material-icons {
-            font-size: 24px;
-        }
-    }
-}
+@import '../assets/stylesheet/header.less';
 </style>
